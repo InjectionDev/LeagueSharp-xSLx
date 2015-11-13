77b63ec749265c9a92d73420eedf2a3273086989
InjectionDev <dfmontenegro@gmail.com>
2015-11-13 16:10:55 -0200

    Ultimate Carry Prevolution

---
+++ LeagueSharp/Ultimate Carry Prevolution/Ultimate Carry Prevolution/Loader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using LeagueSharp;
+
+namespace Ultimate_Carry_Prevolution
+{
+	class Loader
+	{
+		public const string VersionNumber = "0.9";
+
+		public Loader()
+		{
+			Chat.WellCome();
+		}
+	}
+}
